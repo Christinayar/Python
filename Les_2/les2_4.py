4e950a2af2482c0a49d91d3604f7a19908ddcb31
Christina <49560511+Christinayar@users.noreply.github.com>
2020-02-26 15:19:46 +0300

    Update les2_4.py
    Доработано

--- Les_2/les2_4.py
+++ Les_2/les2_4.py
@@ -1,3 +1,3 @@
 string = input('Введите несколько слов: ')
-for el in enumerate(string.split(), 1):
-    print(el)
+for n, i in enumerate(string.split(), 1):
+    print(n, i) if len(i) <= 10 else print(n, (i[:10]))
