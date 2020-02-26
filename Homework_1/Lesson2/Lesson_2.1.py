cf640ff91afda94056c8a97cbbf75691c29219ad
Christina <49560511+Christinayar@users.noreply.github.com>
2020-02-26 16:55:41 +0300

    Update Lesson_2.1.py

--- Homework_1/Lesson2/Lesson_2.1.py
+++ Homework_1/Lesson2/Lesson_2.1.py
@@ -9,3 +9,22 @@ my_list_2 = set(my_list_2)
 same_numbers = (my_list_1 & my_list_2)
 
 print(my_list_1 - same_numbers)
+
+#-----------------------------------------------------------------------
+
+a = [1, 1, 1, 2, 3, 4]
+b = [2, 3]
+
+result = set(a) - set(b)
+print(list(result))
+
+#-----------------------------------------------------------------------
+
+a = [1, 1, 1, 2, 3, 3, 3, 10]
+b = [3, 10, 14]
+
+for number in a[:]:
+    if number in b:
+        a.remove(number)
+
+print(a)
