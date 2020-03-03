a616f45111e60c7ce7984f5ac7c18569e1886787
Christina <49560511+Christinayar@users.noreply.github.com>
2020-03-03 17:56:17 +0300

    Update les_3.6.py

--- GB_Homework_1/Les_3/les_3.6.py
+++ GB_Homework_1/Les_3/les_3.6.py
@@ -7,9 +7,19 @@ print(int_func())
 
 
 
-# Варианты 2 и 3 до конца не вышли -----------------------------------
+# Вариант 2 ------------------------------------
 
-int_func = lambda w: w.title
-print(int_func("hello"))
+int_func = lambda w: print(w.title())
+int_func(input('Введите слово: '))
 
-print((lambda word: word.title)("hello"))
+# Вариант 3 ------------------------------------
+
+def int_func(word):
+    words, result = [], []
+    if len(word) > 0:
+        for i in word.split():
+            words.append(i[0].upper() + i[1:])
+        result = ' '.join(words)
+    return result
+
+print(int_func(input('Введите строку: ')))
