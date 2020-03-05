cde0bcb1b2234768cf5b13d964f96f3fda4db017
Christina <49560511+Christinayar@users.noreply.github.com>
2020-03-05 16:10:22 +0300

    Update les_4.2.py

--- GB_Homework_1/Les_4/les_4.2.py
+++ GB_Homework_1/Les_4/les_4.2.py
@@ -1,3 +1,3 @@
-l = [5, 10, 2, 3, 60, 59]
-new_list = [el for i, el in range(len(l)) if l[i] > l[i - 1]]
+l = [55, 10, 2, 3, 60, 59, 61, 90, 0]
+new_list = [l[i] for i in range(len(l)) if l[i] > l[i - 1]]
 print(new_list)
