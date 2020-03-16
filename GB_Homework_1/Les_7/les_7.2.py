1723547e40aedbe31b86ea4e106222805b367a49
Christina <49560511+Christinayar@users.noreply.github.com>
2020-03-16 15:23:26 +0300

    Update les_7.2.py

--- GB_Homework_1/Les_7/les_7.2.py
+++ GB_Homework_1/Les_7/les_7.2.py
@@ -34,3 +34,31 @@ print(coat.get_square_c())
 
 print(suit.get_sq_full)
 print(suit.get_square_s())
+
+#Var2------------------------------------
+
+from abc import ABC, abstractmethod
+
+class Clothes(ABC):
+    def __init__(self, param):
+        self.param = param
+
+    @abstractmethod
+    def consumption(self):
+        pass
+
+
+class Coat(Clothes):
+    @property
+    def consumption(self):
+        return  f'Consumption of fabric for sewing a coat - {round(self.param / 6.5) + 0.5}'
+
+class Suit(Clothes):
+    @property
+    def consumption(self):
+        return f'Consumption of fabric for sewing a suit - {2 * self.param + 0.3}'
+
+coat = Coat(42)
+suit = Suit(170)
+print(coat.consumption)
+print(suit.consumption)
