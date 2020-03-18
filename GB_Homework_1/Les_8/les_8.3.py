2241982842436445b5e101490544e111c6278cc3
Christina <49560511+Christinayar@users.noreply.github.com>
2020-03-18 16:14:14 +0300

    Update les_8.3.py

--- GB_Homework_1/Les_8/les_8.3.py
+++ GB_Homework_1/Les_8/les_8.3.py
@@ -1,20 +1,21 @@
-class Error(Exception):
-    def __init__(self, *args):
-        self.my_list = []
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
-    def my_input(self):
 
-        while True:
-            try:
-                a = int(input('Введите число: '))
-                self.my_list.append(a)
-                print(f'Список: {self.my_list} \n ')
-            finally:
-                print('Конец')
-    #             if a == 'str':
-    #                 raise Error('Вы ввели недопустимое значение - строку')
-    #         except Error as err:
-    #             print(err)
-    #
-    # if input('Для выхода из программы нажмите "Stop", для продолжения "Enter": '):
-    #     break
+my_list = []
+
+while True:
+    inp_data = input("Введите число: ")
+    if inp_data == "":
+        break
+    try:
+        if inp_data.isdigit():
+            my_list.append(int(inp_data))
+        else:
+            raise OwnError("введено не число")
+    except OwnError as err:
+        print(err)
+        continue
+
+print(my_list)
