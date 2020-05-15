ee67fb5ffe8c0acc13f99935c8807f305889907d
Christina Yarochkina <christina.yarochkina@gmail.com>
2020-05-16 00:01:36 +0300

    Added new exercises

--- Stepik Programming on Python/les_3.py
+++ Stepik Programming on Python/les_3.py
@@ -29,10 +29,30 @@ def modify_list(l):
         else:
             l[i] //= 2
 
+
 # 4-------------------------------------------------------
+
 def update_dictionary(d, key, value):
     if key in d:
         d[key].append(value)
     else:
-        if 2*key not in d : d[2*key] = []
-        update_dictionary(d, 2*key, value)
+        if 2 * key not in d: d[2 * key] = []
+        update_dictionary(d, 2 * key, value)
+
+
+# 5-------------------------------------------------------
+
+import math
+
+r = float(input())
+p = math.pi * r * 2
+print(p)
+
+# 6-------------------------------------------------------
+
+dest = {'север': (0, 1), 'запад': (-1, 0), 'юг': (0, -1), 'восток': (1, 0)}
+n = int(input(''))
+x_list = [(input('')).split(' ') for x in range(n)]
+d = [(dest[x][0] * int(y), dest[x][1] * int(y)) for x, y in x_list]
+res = (sum([x for x, y in d]), sum([y for x, y in d]))
+print(' '.join(map(str, res)))
